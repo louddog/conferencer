60f057c54a205b7644580117bb925014059e49ba
Matt DeClaire <matt@louddog.com>
2011-09-14 17:34:55 -0700

    logo regeneration copy

--- settings/regenerate-logos.php
+++ settings/regenerate-logos.php
@@ -34,6 +34,9 @@ class Conferencer_Rengerate_Logos {
 		<div id="conferencer_regenerate_logos" class="wrap">
 			<h2>Regenerate Logos</h2>
 			
+			<p>When you upload images to WordPress, the system generates several versions of the image at different sizes.  These images are then used in various places in the site.  Conferencer encourages you to upload featured images for conference post types.  These can be used in your templates, for example to show head shots for speaker, or logos for companies.  Depending on your theme, certain sizes are specified for WordPress to generate, like a 75x75 image for thumbnails or a 120x80 image for an archive post image.  Conferencer allows you to define image sizes for a <a href="<?php echo admin_url('widgets.php'); ?>">sponsor slideshow widget</a>.  The problem is that WordPress only generates files when you upload them.  So if change the slideshow image sizes, then any logos previously uploaded do not have the correctly sized image file generated.</p>
+			<p>This page allows you to regenerate your images based on the current image size settings.  So, if you have made recent changes to size settings, you'll need to regenerate your images now.  Don't worry, if you do not run this program, WordPress will still show an image.  It just might not be the size you want until you regenerate.</p>
+			
 			<?php if (count($ids)) { ?>
 				
 				<p class="submit"><input type="submit" class="button-primary" name="conferencer_regenerate_logos" id="conferencer_regenerate_logos_button" value="Regenerate" /></p>
