a7266eed7bd660848ed60208f72bbbf93eba1338
Matt DeClaire <matt@louddog.com>
2011-09-15 12:12:28 -0700

    regeneration progress styles

--- js/admin.js
+++ js/admin.js
@@ -45,25 +45,30 @@ jQuery(function($) {
 		ids: typeof(conferencer_logo_regeneration_ids) != 'undefined' ? conferencer_logo_regeneration_ids : [],
 		ndx: 0
 	};
+	
+	$('#conferencer_regenerate_logos_progress').progressbar({
+		change: function() { $(this).addClass('animated'); },
+		complete: function() { $(this).removeClass('animated'); }
+	}).hide();
 
 	$('#conferencer_regenerate_logos_button').click(function() {
-		$('#conferencer_regenerate_logos_console').html("<p>regenerating logos</p>");
+		$('#conferencer_regenerate_logos_progress').show();
+		$('#conferencer_regenerate_logos_console').html('');
 		logos.ndx = 0;
 		regenerateNextLogo();
 	});
 
 	function regenerateNextLogo() {
+		$('#conferencer_regenerate_logos_progress').progressbar('option', 'value', Math.max(1, 100 * logos.ndx / logos.ids.length));
 		var id = logos.ids[logos.ndx++];
 		if (id) {
 			$.post(ajaxurl, { action: "conferencer_logo_regenerate", id: id }, function(response) {
-				$('#conferencer_regenerate_logos_console').append("<p>" + (response.success ? response.success : response.error) + "</p>");
-				
+				$('#conferencer_regenerate_logos_console').append("<li>" + (response.success ? response.success : response.error) + "</li>");
 				regenerateNextLogo();
 			});
 		} else {
 			$.get(ajaxurl, { action: 'conferencer_logo_regeneration_done' }, function(response) {
 				$('#conferencer_logo_regeneration_needed').slideUp();
-				$('#conferencer_regenerate_logos_console').append("<p>complete</p>");
 			});
 		}
 	}
--- settings/regenerate-logos.php
+++ settings/regenerate-logos.php
@@ -49,7 +49,8 @@ class Conferencer_Rengerate_Logos {
 			<?php if (count($ids)) { ?>
 				
 				<p class="submit"><input type="submit" class="button-primary" name="conferencer_regenerate_logos" id="conferencer_regenerate_logos_button" value="Regenerate" /></p>
-				<div id="conferencer_regenerate_logos_console"><!-- JS --></div>
+				<div id="conferencer_regenerate_logos_progress"></div>
+				<ul id="conferencer_regenerate_logos_console"><!-- JS --></ul>
 				
 				<script> conferencer_logo_regeneration_ids = [<?php echo implode(',', $ids); ?>]; </script>
 				
