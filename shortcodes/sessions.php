78fe52178aa2144770ec7983aec8e131d362042e
Matt DeClaire <matt@louddog.com>
2011-09-21 17:20:25 -0700

    refactored [sessions]: multiple posts, added some options

--- shortcodes/sessions.php
+++ shortcodes/sessions.php
@@ -5,11 +5,12 @@ class Conferencer_Shortcode_Sesssions extends Conferencer_Shortcode {
 	var $shortcode = 'sessions';
 	var $defaults = array(
 		'post_id' => false,
-		'title' => "Sessions",
+		'post_ids' => false,
+		'title' => false,
 		'no_sessions_message' => "There aren't any sessions scheduled for this yet.",
-		'show_speakers' => true,
+		'link_post' => true,
 		'link_sessions' => true,
-		'link_speakers' => true,
+		'title_tag' => 'h3',
 	);
 
 	static $post_types_with_sessions = array('speaker', 'room', 'time_slot', 'track', 'sponsor');
@@ -30,48 +31,72 @@ class Conferencer_Shortcode_Sesssions extends Conferencer_Shortcode {
 		$this->set_options($options);
 		extract($this->options);
 		
-		$post = $post_id ? get_post($post_id) : $GLOBALS['post'];
-		if (!in_array(get_post_type($post), self::$post_types_with_sessions))
-			return "[Shortcode error (sessions): If not used within a Conferencer page (of type speaker, room, time_slot, track, or sponsor), you must provide a post ID using 'post_id'.]";
+		$post_ids = $post_ids ? explode(',', $post_ids) : array();
+		if ($post_id) $post_ids = array_merge($post_ids, explode(',', $post_id));
+		$post_ids = array_unique($post_ids);
+		if (empty($post_ids) && $GLOBAL['post']->ID) $post_id = $GLOBAL['post']->ID;
+		if (empty($post_ids)) return "[Shortcode error (sessions): No session ID provided.]";
 		
-		$sessions = Conferencer::get_sessions($post->ID);
-		Conferencer::attach_speakers($sessions);
-		
-		ob_start(); ?>
-	
-			<?php if (!empty($sessions) || !empty($no_sessions_message)) { ?>
-				<div class="session-list">
-					<h2><?php echo $title; ?></h2>
-				
-					<?php if (empty($sessions)) { ?>
-						<p><?php echo $no_sessions_message; ?></p>
-					<?php } else { ?>
-						<ul>
-							<?php foreach ($sessions as $session) { ?>
-								<li>
-									<?php
-										$html = $session->post_title;
-										if ($link_sessions) $html = "<a href='".get_permalink($session->ID)."'>$html</a>";
-										if ($show_speakers) {
-											if ($post->post_type == 'speaker') {
-												$speakers = $session->speakers;
-												foreach ($speakers as $id => $speaker) {
-													if ($id == $post->ID) unset($speakers[$id]);
-												}
-												if (count($speakers)) $html .= ", with ".comma_separated($speakers, $link_speakers);
-											} else {
-												$html .= ", by ".comma_separated($session->speakers, $link_speakers);
-											}
+		$content = '';
+
+		foreach ($post_ids as $post_id) {
+			$post = get_post($post_id);
+			
+			if (!$post) {
+				$content .= "[Shortcode error (sessions): $post_id is not a valid post ID.]";
+			} else if (!in_array(get_post_type($post), self::$post_types_with_sessions)) {
+				$content .= "[Shortcode error (sessions): <a href='".get_permalink($post->ID)."'>$post->post_title</a> is not the correct type of post.  It is of type $post->post_type.  If not used within a Conferencer page of type speaker, room, time_slot, track, or sponsor), you must provide a post ID using 'post_id'.]";
+			} else {
+				$sessions = Conferencer::get_sessions($post->ID);
+				Conferencer::attach_speakers($sessions);
+
+				ob_start(); ?>
+
+					<?php if (!empty($sessions) || !empty($no_sessions_message)) { ?>
+						<div class="session-list">
+							<<?php echo $title_tag; ?>>
+								<?php
+									if (!$title) {
+										echo "Sessions ";
+
+										switch ($post->post_type) {
+											case 'speaker': echo 'with'; break;
+											case 'room': echo 'in'; break;
+											case 'time_slot': echo 'at'; break;
+											case 'track': echo 'in'; break;
+											case 'sponsor': echo 'sponsored by'; break;
+											default: echo "for";
 										}
-										echo $html;
-									?>
-								</li>
+										
+										$html = $post->post_title;
+										if ($link_post) $html = "<a href='".get_permalink($post->ID)."'>$html</a>";
+										echo " ".$html;
+									} else echo $title;
+								?>
+							</<?php echo $title_tag; ?>>
+
+							<?php if (empty($sessions)) { ?>
+								<p><?php echo $no_sessions_message; ?></p>
+							<?php } else { ?>
+								<ul>
+									<?php foreach ($sessions as $session) { ?>
+										<li>
+											<?php
+												$html = $session->post_title;
+												if ($link_sessions) $html = "<a href='".get_permalink($session->ID)."'>$html</a>";
+												echo $html;
+											?>
+										</li>
+									<?php } ?>
+								</ul>
 							<?php } ?>
-						</ul>
+						</div>
 					<?php } ?>
-				</div>
-			<?php } ?>
-	
-		<?php return ob_get_clean();
+
+				<?php $content .= ob_get_clean();
+			}
+		}
+		
+		return $content;
 	}
 }
