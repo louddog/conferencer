25d67b4b6b64b7b0e9af6d06bb771b999944bfbb
Matt DeClaire <matt@louddog.com>
2011-12-08 11:29:50 -0800

    move code into enclosure

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+var conferencer;
+
 jQuery(function($) {
 	// alert user when they change logo sizes to regenerate
 	
@@ -17,36 +19,45 @@ jQuery(function($) {
 			$('#conferencer_logo_regeneration_needed').slideDown();
 		}
 	});
+	
+	conferencer = {
+		tinymce_button: function(opts) {
+			var options = {
+				slug: false,
+				title: '',
+				image: false,
+				onclick: false
+			};
+
+			if (opts) $.extend(options, opts);
+			if (!options.slug) return false;
+			options.slug = 'conferencer_' + options.slug;
+
+			tinymce.create('tinymce.plugins.' + options.slug, {
+				init: function(editor, url) {
+					editor.addButton(options.slug, {
+						title: options.title,
+						image: url + '/' + options.image,
+						onclick: function() {
+							options.onclick(editor, url);
+						}
+					});
+				},
+				getInfo: function() {
+					return {
+						longname: options.title,
+						author: "Loud Dog",
+						authorurl: 'http://conferencer.louddog.com',
+						infourl: '',
+						version: "1.0"
+					};
+				},
+				createControl: function(n, cm) {
+					return null;
+				}
+			});
+
+			tinymce.PluginManager.add(options.slug, tinymce.plugins[options.slug]);
+		}
+	};
 });
-
-var conferencer = {
-	tinymce_button: function(opts) {
-		var options = {
-			slug: false,
-			title: '',
-			image: false,
-			onclick: false
-		};
-
-		if (opts) jQuery.extend(options, opts);
-		if (!options.slug) return false;
-		options.slug = 'conferencer_' + options.slug;
-
-		tinymce.create('tinymce.plugins.' + options.slug, {
-			init: function(editor, url) {
-				editor.addButton(options.slug, {
-					title: options.title,
-					image: url + '/' + options.image,
-					onclick: function() {
-						options.onclick(editor);
-					}
-				});
-			},
-			createControl: function(n, cm) {
-				return null;
-			}
-		});
-
-		tinymce.PluginManager.add(options.slug, tinymce.plugins[options.slug]);
-	}
-};
